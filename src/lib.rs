7791ebd0af94170c3dd1b7b4b99b8468fe3a5520
Mettwasser <kaicrafter2000@gmail.com>
2026-08-09 21:07:18 +0200

    fix: error re-export

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,17 @@
-pub use minimessage_impl::{
-    error::{Error, Result},
-    parser::{Expression, Node, Parser},
-    style,
-    token::Token,
-    tokenizer::Tokenizer,
-};
+pub mod parser {
+    pub use minimessage_impl::{
+        parser::{Expression, Node, Parser},
+        style,
+        token::Token,
+        tokenizer::Tokenizer,
+    };
+}
 pub use minimessage_macro::minimessage;
-pub use minimessage_runtime::{ArgumentCollection, FormatArgs, deserialize, deserialize_with_args};
+pub use minimessage_runtime::{
+    ArgumentCollection,
+    Error,
+    FormatArgs,
+    Result,
+    deserialize,
+    deserialize_with_args,
+};
